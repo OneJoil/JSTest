1682d7b87202bbb0813b717635b545b6c68fb376
Anessan <vova.tverdov@gmail.com>
2022-07-10 13:44:25 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// ШПАРГАЛКА 
+
+// '++' - увеличение на 1
+// '--' - уменьшение на 1
+
+let inscr = 10,
+    descr = 10;
+
+++inscr;
+--descr;
+
+console.log(inscr);
+console.log(descr);
+
+// && - И
+// || - ИЛИ
