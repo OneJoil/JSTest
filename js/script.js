508b0d568ab98a39827dd99a4aa77992437e529a
Anessan <vova.tverdov@gmail.com>
2022-07-11 13:51:46 +0300

    61

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+'use strict';
+
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
@@ -8,12 +24,15 @@ const personalMovieDB = {
     privat: false
 };
 
-const whatsMovie = prompt('Один из последних просмотренных фильмов?', ''),
-      rateMovie = prompt('На сколько оцените его?', ''),
-      whatsMovie1 = prompt('Один из последних просмотренных фильмов?', ''),
-      rateMovie1= prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+    const whatsMovie = prompt('Один из последних просмотренных фильмов?', ''),
+          rateMovie = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[whatsMovie] = rateMovie;
-personalMovieDB.movies[whatsMovie1] = rateMovie1;
-
-console.log(personalMovieDB);
+    if ( whatsMovie != null && rateMovie != null && whatsMovie != '' && rateMovie != '' && whatsMovie.length < 50 && rateMovie.length < 50) {
+        personalMovieDB.movies[whatsMovie] = rateMovie;
+        console.log('done');
+    } else {
+        console.log('done');
+        i--;
+    }
+}
